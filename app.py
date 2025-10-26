585cbdebe545160b79bd387c3584c82dd7047004
Joynase <dimejoynase@isufst.edu.ph>
2025-10-26 15:50:52 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,12 +3,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-return "Welcome to my Flask API!"
+    return "Welcome to my Flask API!"
 
 @app.route('/student')
 def get_student():
-return jsonify({
-"name": "Your Name",
-"grade": 10,
-"section": "Zechariah"
-})
+    return jsonify({
+        "name": "Your Name",
+        "grade": 10,
+        "section": "Zechariah"
+    })
+
+if __name__ == '__main__':
+    app.run()
